b95a361174d1740fad14885df30b52314b4a53a9
wvha <Wvha17@gmail.com>
2018-09-17 22:21:03 -0700

    added queue and breadth first solution

--- 226-invertBinaryTree.js
+++ 226-invertBinaryTree.js
@@ -1,6 +1,33 @@
+// simple way. recursive
+
 var invertTree = function(root) {
   if (root) {
     [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  }
+  return root;
+}
+
+// using a queue and breadth first
+
+var invertTree2 = function(root) {
+  if (root === null) {
+    return null;
+  }
+
+  let queue = [];
+  queue.push(root);
+  while (queue.length > 0) {
+    let current = queue.shift();
+    let temp = current.left;
+    current.left = current.right;
+    current.right = temp;
+
+    if (current.left !== null) {
+      queue.push(current.left);
+    }
+    if (current.right !== null) {
+      queue.push(current.right);
+    }
   }
   return root;
 }

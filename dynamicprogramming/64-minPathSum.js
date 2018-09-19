341f6de9587dbebd824ebc6ee0a7fa663586271a
wvha <Wvha17@gmail.com>
2018-09-19 01:30:44 -0700

    complete ish min path sum

---
+++ dynamicprogramming/64-minPathSum.js
@@ -0,0 +1,25 @@
+function minPathSum(grid) {
+  let height = grid.length;
+  let width = grid[0].length;
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (i !== 0 && j !== 0) {
+        grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+      } else if (i !== 0) {
+        grid[i][j] += grid[i-1][j];
+      } else if (j !== 0) {
+        grid[i][j] += grid[i][j-1];
+      }
+    }
+  } return grid[height-1][width-1];
+}
+
+/*
+how to explain this lol
+
+
+
+
+
+*/

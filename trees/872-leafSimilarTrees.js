865b6bc2653552c783df39f4607b5ab826881299
wvha <Wvha17@gmail.com>
2018-09-24 23:40:50 -0700

    complete leaf similar trees

--- trees/872-leafSimilarTrees.js
+++ trees/872-leafSimilarTrees.js
@@ -18,11 +18,33 @@ input is 2 trees
 constraints
 e??
 
-
+recurse until there's no children nodes
+remember, left to right
+when you reach a leaf, push to array
+do the same for root 2
+if two ttrees the same, then return true
 
 
  */
 
 var leafSimilar = function(root1, root2) {
+  let tree1 = [];
+  let tree2 = [];
 
+  function search(node, array) {
+    if (!node.left && !node.right) {
+      array.push(node.val);
+    }
+
+    if (node.left) {
+      search(node.left, array);
+    }
+    if (node.right) {
+      search(node.right, array);
+    }
+  }
+
+  search(root1, tree1);
+  search (root2, tree2);
+  return JSON.stringify(tree1) === JSON.stringify(tree2);
 };

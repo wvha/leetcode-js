6613675a293153f010c952f2dbc890e0de151837
wvha <Wvha17@gmail.com>
2018-09-24 23:35:14 -0700

    start leaf similar trees

---
+++ trees/872-leafSimilarTrees.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+
+ /*
+output is an array with leaf value sequence
+input is 2 trees
+
+constraints
+e??
+
+
+
+
+ */
+
+var leafSimilar = function(root1, root2) {
+
+};
